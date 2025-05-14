9dfcc2c8dab2805b01cac281a87d8ea587de0e64
Shreya Khantal <khantalshreya@gmail.com>
2025-05-15 01:37:52 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -43,9 +43,9 @@ app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Change if using another provider
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'thats.a.real.spell@gmail.com' 
-app.config['MAIL_PASSWORD'] = 'nzpqxnhvchmrujur'
-app.config['MAIL_DEFAULT_SENDER'] = 'thats.a.real.spell@gmail.com'
+app.config['MAIL_USERNAME'] = 'MAIL_USERNAME' 
+app.config['MAIL_PASSWORD'] = 'MAIL_PASSWORD'
+app.config['MAIL_DEFAULT_SENDER'] = 'MAIL_SENDER'
 
 # ==============================
 # Initialize Extensions
